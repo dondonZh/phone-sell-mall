a76f3de3624feaff4c401c4994360ced4cc19194
zhoudd <976201076@qq.com>
2019-07-16 17:31:55 +0800

    404 准备改造嵌套路由

--- src/components/bottomBar.vue
+++ src/components/bottomBar.vue
@@ -2,35 +2,35 @@
   <div class="bottomBar">
     <ul>
       <li>
-        <div class="bottom">
-        <iconSvg  icon-class="xihuan" iconSize="small" />
+        <div class="bottom" @click="addActive(menus[0])">
+        <iconSvg  :icon-class="menus[0]"  :class="{active:menus[0]==choosed}" iconSize="small" />
         <div>收藏</div>
         </div>
       </li>
       <li>
-        <div class="bottom">
-        <iconSvg   icon-class="gouwuche" iconSize="small" />
+        <div class="bottom" @click="addActive(menus[1])">
+        <iconSvg   :icon-class="menus[1]" :class="{active:menus[1]==choosed}" iconSize="small" />
         <div>号码</div>
         </div>
       </li>
       <li>
-        <div   class="circle inMiddle">
+        <div   class="circle inMiddle" @click="addActive(menus[2])" >
         </div>
-        <iconSvg   class="inMiddle add" icon-class="add" iconSize="middle" />
+        <iconSvg   class="inMiddle add"  :icon-class="menus[2]" iconSize="middle"  />
         <div class="bottom">
         <div class="middleFind">发现</div>
         </div>
       </li>
       <li>
-        <div class="bottom">
-          <iconSvg  icon-class="sousuo"  iconSize="small"/>
+        <div class="bottom"  @click="addActive(menus[3])" >
+          <iconSvg  :icon-class="menus[3]"  :class="{active:menus[3]==choosed}" iconSize="small" />
           <div>信息</div>
         </div>
 
       </li>
       <li>
-        <div class="bottom">
-        <iconSvg   icon-class="kefu" iconSize="small" />
+        <div class="bottom" @click="addActive(menus[4])" >
+        <iconSvg   :icon-class="menus[4]" :class="{active:menus[4]==choosed}" iconSize="small" />
         <div >客服</div>
         </div>
       </li>
@@ -39,9 +39,22 @@
 </template>
 
 <script>
-  export default {
-    name: 'bottomBar'
+  //TODO 404   嵌套路由
+// TODO 引入常量
+export default {
+  name: 'bottomBar',
+  data () {
+    return {
+      menus: ['xihuan', 'gouwuche', 'add', 'sousuo', 'kefu'],
+      choosed: ''
+    }
+  },
+  methods: {
+    addActive (className) {
+      this.choosed = className
+    }
   }
+}
 </script>
 
 <style scoped>
@@ -90,6 +103,14 @@
   .add{
     z-index: 4;
     bottom:45px;
+  }
+  /deep/ .icon{
+    transition: all .5s;
+  }
+  .active{
+    /**translate3d 硬件加速**/
+    transform: scale(1.2) translate3d(0,-30%,0);
+    background:url("../assets/img/y.png") no-repeat center/100% 100%;
   }
 
 </style>
