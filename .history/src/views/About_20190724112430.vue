73e22c3443c3ad059fbb44a5649e4c6c13a33587
zhoudd <976201076@qq.com>
2019-07-24 11:44:07 +0800

    vscode提交

---
+++ .history/src/views/About_20190724112430.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+      <div>{{env}}</div>
+      <div>{{env}}</div>
+      <div>{{env}}</div>
+      <div>{{env}}</div>
+      <div>{{env}}</div>
+      <div>{{env}}</div>
+      <div>{{env}}</div>
+      <div>{{env}}</div>
+      <div>{{env}}</div>
+<div></div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      env: process.env.NODE_ENV
+    }
+  }
+}
+
+</script>
+<style>
+  .container {
+    width: 100%;
+    display: grid;
+    grid-template-columns:repeat(auto-fit, minmax(100px, 1fr));
+    grid-template-rows: repeat(2, 50px);
+  }
+  .container  div:nth-child(even){
+    background-color: yellow;
+  }
+  .container  div:nth-child(odd){
+    background-color: green;
+  }
+</style>
